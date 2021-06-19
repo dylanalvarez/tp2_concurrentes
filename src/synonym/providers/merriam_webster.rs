d06dcf212b769c504706ce4d073a57ec8f954515
dylan <dylanalvarez1995@gmail.com>
2021-06-19 20:20:07 -0300

    Add missing provider

--- src/synonym/providers/merriam_webster.rs
+++ src/synonym/providers/merriam_webster.rs
@@ -1,12 +1,12 @@
 use crate::synonym::providers::base;
 use crate::synonym::providers::base::Provider::MerriamWebster;
 
-pub fn synonyms(word: &str) -> Vec<String> {
+pub fn synonyms(word: &str) -> Result<Vec<String>, String> {
     return base::synonyms(word, MerriamWebster);
 }
 
 // Mejorar esta bosta
-pub fn raw_response_to_synonyms(body_to_scrap: String) -> Vec<String> {
+pub fn raw_response_to_synonyms(body_to_scrap: String) -> Result<Vec<String>, String> {
     let mut tmp_synonyms_vec: Vec<&str> = body_to_scrap.rsplit("mw-list").collect();
     let results = tmp_synonyms_vec[tmp_synonyms_vec.len() - 2].to_string();
     tmp_synonyms_vec = results.split("/thesaurus/").collect();
@@ -19,6 +19,5 @@ pub fn raw_response_to_synonyms(body_to_scrap: String) -> Vec<String> {
         synonyms.push(synonym[0].replace("%20", " "));
     }
 
-    println!("[merriam-webster] Found synonyms: {:?}", synonyms);
-    synonyms
+    Ok(synonyms)
 }
--- src/synonym/providers/thesaurus2.rs
+++ src/synonym/providers/thesaurus2.rs
@@ -1,10 +1,18 @@
 use crate::synonym::providers::base::Provider::Thesaurus2;
 use crate::synonym::providers::base;
 
-pub fn synonyms(word: &str) -> Vec<String> {
-    return base::synonyms(word, Thesaurus2);
+pub fn synonyms(word: &str) -> Result<Vec<String>, String> {
+    base::synonyms(word, Thesaurus2)
 }
 
-pub fn raw_response_to_synonyms(_raw_response: String) -> Vec<String> {
-    return Vec::new();
+pub fn raw_response_to_synonyms(raw_response: String) -> Result<Vec<String>, String> {
+    let synonyms: Vec<String> = raw_response
+        .split(r#"<div data-testid="word-grid-container""#)
+        .collect::<Vec<&str>>()[1]
+        .split("</ul>")
+        .collect::<Vec<&str>>()[0]
+        .split("<!-- -->")
+        .map(|x| x.rsplit(">").collect::<Vec<&str>>()[0].to_string())
+        .collect();
+    Ok(synonyms[0..synonyms.len() - 1].to_owned())
 }
