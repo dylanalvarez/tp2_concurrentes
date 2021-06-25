cd17d47b2292c6bb1b30748c26dde7cc289bdd67
DonatoJP <jpdonato@balloon-group.com>
2021-06-25 19:06:51 -0300

    Fix no result found parser Thesaurus

--- src/synonym/providers/thesaurus.rs
+++ src/synonym/providers/thesaurus.rs
@@ -1,6 +1,10 @@
 pub fn raw_response_to_synonyms(raw_response: String) -> Result<Vec<String>, String> {
+
+    if raw_response.contains("No results found") {
+        return Ok(vec![]);
+    }
+
     let tmp_synonyms_vec = raw_response.split("<div class=\"single-synonym-wrapper\"").collect::<Vec<&str>>()[1];
-    
     let tmp_synonyms_column = tmp_synonyms_vec.split("<").collect::<Vec<&str>>()[1..].join("<");
     let tmp_single_synonyms: Vec<String> = tmp_synonyms_column
         .split("<div class=\"single-synonym\"")
@@ -47,6 +51,22 @@ mod tests {
             Ok(raw_html) => {
                 let result = SingleSynonym {raw_html: raw_html}.get_synonym();
                 assert_eq!(result, "motor-car")
+            }
+        };
+    }
+
+    #[test]
+    fn test_empty_synonyms_result() {
+        let path = Path::new("src/synonym/providers/tests/empty_search.html");
+        match read_to_string(path) {
+            Err(e) => {
+                print!("{:?}", e);
+                panic!("Cant open file");
+            }
+
+            Ok(raw_html) => {
+                let result = raw_response_to_synonyms(raw_html);
+                assert_eq!(result, Ok(vec![]))
             }
         };
     }
