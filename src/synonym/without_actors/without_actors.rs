df35ed3b6a4173cffa32239df70703f55bb288eb
DonatoJP <jpdonato@balloon-group.com>
2021-06-25 20:41:48 -0300

    More logs without_actors

--- src/synonym/without_actors/without_actors.rs
+++ src/synonym/without_actors/without_actors.rs
@@ -6,7 +6,7 @@ use std::thread;
 use std::sync::{mpsc, Arc, Mutex, Condvar};
 use std::collections::HashMap;
 use std_semaphore::Semaphore;
-use std::time::Duration;
+use std::time::{Duration};
 use std::thread::sleep;
 use crate::{ResultBuilderMessage, synonym};
 use crate::ResultBuilderMessage::NoMoreSynonyms;
@@ -75,7 +75,12 @@ pub fn without_actors(filename: &String, max_concurrent_requests: usize, min_sec
 
     let mut synonym_fetcher_threads = Vec::new();
 
+    let log_sender_provider = mpsc::Sender::clone(&log_sender);
     for provider in [Thesaurus, Thesaurus2, MerriamWebster].iter() {
+        handle_log(
+            &log_sender_provider,
+            format!("[Provider {:?}] Starting...", provider)
+        );
         let time_between_requests_has_elapsed_condvar = Arc::new((Mutex::new(true), Condvar::new()));
         let (sleeper_sender, sleeper_receiver) = mpsc::channel::<()>();
         let condvar1 = time_between_requests_has_elapsed_condvar.clone();
@@ -99,6 +104,10 @@ pub fn without_actors(filename: &String, max_concurrent_requests: usize, min_sec
                 for word in lines {
                     match word {
                         Ok(word) => {
+                            handle_log(
+                                &log_sender_provider,
+                                format!("[Provider {:?}] Fetching synonyms for word: {:?}", provider, word)
+                            );
                             let result_builder_sender = mpsc::Sender::clone(&result_builder_sender);
                             let sleeper_sender = mpsc::Sender::clone(&sleeper_sender);
                             let semaphore = max_concurrent_requests_semaphore.clone();
@@ -107,28 +116,57 @@ pub fn without_actors(filename: &String, max_concurrent_requests: usize, min_sec
                                 synonym::providers::base::synonyms(word.as_str(), provider, result_builder_sender, &semaphore, &*condvar2, sleeper_sender);
                             }));
                         }
-                        Err(error) => { panic!("{}", error) }
+                        Err(error) => { 
+                            handle_log(
+                                &log_sender_provider,
+                                format!("[Provider {:?}] Failed to read lines from file: {:?}", provider, error)
+                            );
+                            panic!("{}", error) 
+                        }
                     };
                 }
             }
-            Err(error) => {panic!("Couldn't read words.txt: {}", error)}
+            Err(error) => {
+                handle_log(
+                    &log_sender_provider,
+                    format!("[Provider {:?}] Couldn't read words.txt: {}", provider, error)
+                );
+                panic!("Couldn't read words.txt: {}", error)
+            }
         };
     }
 
+    handle_log(
+        &log_sender,
+        format!("[Main] Starting to join synonym fetchers")
+    );
     for thread in synonym_fetcher_threads {
         match thread.join() {
             Ok(_) => {}
             Err(_) => {panic!("Couldn't join a synonym fetcher")}
         }
     }
+
     match result_builder_sender.send(NoMoreSynonyms) {
-        Ok(_) => {}
+        Ok(_) => {
+            handle_log(
+                &log_sender,
+                format!("[Main] No more fetchers working. NoMoreSynonyms sent")
+            );
+        }
         Err(_) => {panic!("Lost connection with result_builder")}
     }
+
     match result_builder.join() {
         Ok(result) => {
             match result {
-                Ok(result) => { println!("{:?}", result) }
+                Ok(result) => { 
+                    handle_log(
+                        &log_sender,
+                        format!("[Main] Final result {:?}", result)
+                    );
+                    println!("{:?}", result) 
+                }
                 Err(result_builder_error) => { println!("{:?}", result_builder_error) }
             }
         }
