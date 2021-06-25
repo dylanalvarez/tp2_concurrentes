433c701f37da3a64a686b8ccdf70c9d70f4e1bf5
Tomas Botalla <tbotalla@gmail.com>
2021-06-24 23:19:47 -0300

    Mejora en parser merriam_webster.rs
    + todo con split, map, filter, collect
    + considerado escenario palabras sin sinonimos

--- src/synonym/providers/merriam_webster.rs
+++ src/synonym/providers/merriam_webster.rs
@@ -1,16 +1,18 @@
-// Mejorar esta bosta
 pub fn raw_response_to_synonyms(body_to_scrap: String) -> Result<Vec<String>, String> {
-    let mut tmp_synonyms_vec: Vec<&str> = body_to_scrap.rsplit("mw-list").collect();
-    let results = tmp_synonyms_vec[tmp_synonyms_vec.len() - 2].to_string();
-    tmp_synonyms_vec = results.split("/thesaurus/").collect();
-    tmp_synonyms_vec = Vec::from(&tmp_synonyms_vec[1..]);
-
-    let mut synonyms = Vec::new();
-    for i in &tmp_synonyms_vec {
-        let synonym: Vec<&str> = i.split("\"").collect();
-        // TODO: user % ASCII decoding sin crate externo
-        synonyms.push(synonym[0].replace("%20", " "));
+    if body_to_scrap.contains("missing-query") {
+        return Ok(Vec::new());
     }
+
+    let synonyms: Vec<String> = body_to_scrap
+        .split("mw-list")
+        .collect::<Vec<&str>>()[1]
+        .split("/thesaurus/")
+        .collect::<Vec<&str>>()
+        .iter()
+        .filter(|x| x.contains("</a>"))
+        .map(|x| x.replace("%20", " "))
+        .map(|x| x.split("\"").collect::<Vec<&str>>()[0].to_string())
+        .collect();
 
     Ok(synonyms)
 }
